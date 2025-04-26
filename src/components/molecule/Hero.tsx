c31a1a3a0832f6feb7be9756a1ff304c9928a243
Abdulmujib <asiyanbiakorede@gmail.com>
2025-04-26 11:07:43 +0100

    WIP:hero section

--- src/components/molecule/Hero.tsx
+++ src/components/molecule/Hero.tsx
@@ -5,49 +5,65 @@ import { useState } from "react";
 
 function Hero() {
   const [location,setLocation] = useState(false)
+  const [searchText,setSearchText] = useState('')
 
   const handleShowLocations = () => {
     setLocation(prev => !prev)
   }
+  const handleSearch = () => {
+    setSearchText('')
+    console.log('button pressed',{searchText})
+  }
   return (
-    <div className="heroBackground text-[#fff] h-[36rem] flex items-start justify-center flex-col font-bold text-5xl   gap-14 w-full">
-      <section className="pl-[13rem] leading-[4.4rem]">
+    <div className="heroBackground  h-[36rem] flex items-start justify-center flex-col  gap-14 w-full">
+      <section className="pl-[13rem] leading-[4.4rem] text-[#fff]  text-5xl font-bold">
         <p>Dont miss out!</p>
         <p>
           Explore the <span className="text-tifyYellow">vibrant events</span>{" "}
           happening locally and globally.{" "}
         </p>
       </section>
-      <section className="w-full flex justify-between ">
-        <form>
-          <div>
+      <section className="w-full flex justify-center ">
+        <form className="bg-white flex border-r-[2px] border-r-[#BBBBBB] w-[45%] justify-between px-6 py-4.5 rounded-l-lg text-xl parentEl">
+          <div className="flex gap-6 w-full">
             <Image
-              src="/Ellipse 1.svg"
+              src="/Search icon.svg"
               alt="search-image"
               width={25}
               height={25}
             />
-            <input type="text" />
+            <input
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+              className="outline-none w-[80%] search"
+              placeholder="Search Events, Categories, Location,..."
+              type="text"
+            />
           </div>
           <Image
+            className="cancel-img"
+            onClick={handleSearch}
             alt="cancel-img"
             src={"/Close button.svg"}
             width={30}
             height={30}
           />
         </form>
-        <main>
-          <div onClick={handleShowLocations}>
-            <div>
+        <main
+          className="bg-white w-[20%] relative flex items-center rounded-r-lg "
+          onClick={handleShowLocations}
+        >
+          <div className="flex justify-between w-full mx-6">
+            <div className="flex gap-3.5">
               <Image
                 src={"/Location icon.svg"}
                 alt="location-image"
                 width={22}
                 height={30}
               />
-              <span>Mumbai</span>
+              <input className="font-medium text-xl outline-none" placeholder="Mumbai" />
             </div>
-            {location && (
+            {!location && (
               <Image
                 alt="arrow-svg"
                 src={"/Vector 3.svg"}
@@ -55,7 +71,7 @@ function Hero() {
                 height={9}
               />
             )}
-            {!location && (
+            {location && (
               <Image
                 alt="arrow-svg"
                 src={"/Vector 3 (1).svg"}
@@ -64,6 +80,28 @@ function Hero() {
               />
             )}
           </div>
+          {location && (
+            <div className="absolute border-t-[2px] border-t-[#5A5A5A] bottom-[-9.15em] bg-white w-full flex flex-col ">
+              <div className="border-b-[1px] border-b-[#5A5A5A] hover:bg-[#E6E5E7] w-full flex gap-3.5 px-4 py-6 items-center text-l ">
+                <Image
+                  src={"/Vector.svg"}
+                  alt="destination"
+                  width={25}
+                  height={25}
+                />
+                <span>Detect Current Location</span>
+              </div>
+              <div className=" hover:bg-[#E6E5E7] w-full flex gap-3.5 px-4 py-6 items-center text-l ">
+                <Image
+                  src={"/Vector (1).svg"}
+                  alt="destination"
+                  width={25}
+                  height={25}
+                />
+                <span>Online</span>
+              </div>
+            </div>
+          )}
         </main>
       </section>
     </div>
